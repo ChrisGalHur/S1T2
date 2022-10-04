963d01990482d3c07eba150f7a7935965e834239
Christian <christian.galvez.hurtado@hotmail.com>
2022-10-04 13:18:21 +0200

    Casi acabado el N2

--- S1T2N2/Entrada.java
+++ S1T2N2/Entrada.java
@@ -1,5 +1,159 @@
 package S1T2N2;
 
-public class Entrada {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Entrada extends Exception{
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	private static String msjeEntrada;
+	
+	private static int inte, intei;
+	private static float flo, floi;
+	private static double dou, doui;
+	private static String cha, chai;
+	private static String str, stri;
+	
+	//Byte
+	public static byte leerByte(String msjeEntrada) {
+		boolean salida = false;
+		byte byt = 0;
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				byt = scan.nextByte();
+				System.out.println("Bien!");
+				salida = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Tenía que ser un Byte");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return byt;
+	}
+	
+	//Integer
+	public static Integer leerInt(String msjeEntrada) {
+		boolean salida = false;
+		int inte = 0;
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				inte = scan.nextInt();
+				System.out.println("Bien!");
+				salida = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Eso no es un Integer...");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return inte;
+	}
+	
+	//Float
+	public static float leerFloat(String msjeEntrada) {
+		boolean salida = false;
+		float flo = 0;
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				flo = scan.nextFloat();
+				System.out.println("Bien!");
+				salida = true;
+			}catch (InputMismatchException e) {
+				System.out.println("Error de formato");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return flo;
+	}
+	
+	//Double
+	public static double leerDouble(String msjeEntrada) {
+		boolean salida = false;
+		double dou = 0;
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				dou = scan.nextDouble();
+				System.out.println("Bien!");
+				salida = true;
+			}catch (InputMismatchException e) {
+				System.out.println("He dicho un Double");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return dou;
+	}
+	
+	//Char
+	public static String leerChar(String msjeEntrada) {
+		boolean salida = false;
+		String cha = null;
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				cha = scan.next();
+					if(cha.length() > 0 && cha.length() < 2) {
+						System.out.println("Vale, seguimos!!");
+						salida = true;
+					}else {
+						System.out.println("Es UN char...");
+					}
+			}catch (Exception e) {
+				System.out.println("Pista: Es un char...");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return cha;
+	}
+	
+	//String
+	public static String leerString(String msjeEntrada) {
+		boolean salida = false;
+		String str = "";
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				str = scan.next();
+				System.out.println("Has acabado, felicidades!!!");
+				salida = true;
+			}catch (Exception e) {
+				System.out.println("Este era facil...");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return str;
+	}
+	
+	//S o N
+	public static boolean leerSiONo(String msjeEntrada) {
+		boolean salida = false;
+		boolean comprobante = false;
+		char siONo = 0;
+		
+		do {
+			try {
+				System.out.println(msjeEntrada);
+				siONo = scan.next().toUpperCase().charAt(0);
+				comprobante = String.valueOf(siONo).equals("S");//si l’usuari/*ària introdueix “s”, retorna “true”, si l’usuari introdueix “n”, retorna “false”.
+				salida = true;
+			}catch (Exception e) {
+				System.out.println("Eso no es una respuesta!!");
+			}
+			scan.nextLine();
+		}while(salida == false);
+		return comprobante;
+	}
+	
 }
+	
+	
+
--- S1T2N2/Main.java
+++ S1T2N2/Main.java
@@ -28,16 +28,27 @@ public class Main {
 		Mètodes a implantar capturant l’excepció de la classe Exception:
 		public static char llegirChar(*String missatge);
 		public static String llegirString(*String missatge);
-		public static boolean llegirSiNo(*String missatge), si l’usuari/*ària introdueix “s”, retorna “true”, si l’usuari introdueix “n”, retorna “false”.
-		Neteja Copia el text*/
+		public static boolean llegirSiNo(*String missatge), si l’usuari/*ària introdueix “s”, retorna “true”, si l’usuari introdueix “n”, retorna “false”.*/
 		
-		int prueba;
+		System.out.println("EXAMEN DE PROGRAMACIÓN\n"
+				+ "");
+		//Byte
+//		System.out.println(Entrada.leerByte("Dime un Byte:"));
+		//Integer
+//		System.out.println(Entrada.leerInt("Dame un Integer:"));
+		//Float
+//		System.out.println(Entrada.leerFloat("Un Float:"));
+		//Double
+//		System.out.println(Entrada.leerDouble("Ahora un Double"));
+		//Char
+//		System.out.println(Entrada.leerChar("Dame un char:"));
+		//String
+//		System.out.println(Entrada.leerString("Dime un String:"));
+		//SiNo
+		System.out.println(Entrada.leerSiONo("Te ha gustado el examen? S/N"));
 		
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Dame un número:");
-		prueba = scan.nextInt();
-		System.out.println(prueba);
+		
 		
 	}
 
